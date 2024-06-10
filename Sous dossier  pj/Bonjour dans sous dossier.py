00a20bc42e8f2ab56e6e6f2d4f6a6dd98e73037d
Maurice <152451905+MauriceLacourt@users.noreply.github.com>
2024-06-10 17:10:30 +0200

    Néttoyage

---
+++ Sous dossier  pj/Bonjour dans sous dossier.py
@@ -0,0 +1,9 @@
+print("Bonjour, monde !")
+livre = "Gatsby le Magnifique"
+print ("coucou101")
+print ("coucou6")
+print ("coucou9999")
+print ("Philippe")
+print ("Martin")
+print ("pifou")
+print ("pifou3")
--- scrap.py
+++ scrap.py
@@ -3,20 +3,30 @@ from bs4 import BeautifulSoup
 import re
 
 def extract_emails(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
     emails = []
 
-    for element in soup.find_all(name='a'):
-        text = element.text
-        match = re.search(r'[\w\.-]+@[\w\.-]+\.\w+', text)
-        if match:
-            emails.append(match.group())
+    # Récupère la liste de toutes les pages du site
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    urls = [a['href'] for a in soup.find_all(name='a')]
+
+    # Itère sur toutes les pages
+    for u in urls:
+        response = requests.get(u)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Recherche les adresses e-mail sur la page
+        for element in soup.find_all(name='a'):
+            text = element.text
+            match = re.search(r'[\w\.-]+@[\w\.-]+\.\w+', text)
+            if match:
+                emails.append(match.group())
 
     return emails
 
 
 if __name__ == '__main__':
-    url = 'https://www.example.com'
+    url = input('Veuillez entrer l\'adresse du site : ')
     emails = extract_emails(url)
     print(emails)
+
