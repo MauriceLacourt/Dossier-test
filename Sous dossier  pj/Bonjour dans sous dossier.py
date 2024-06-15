5557b48ec353120b08dc2e138b0fa81751114f20
Maurice <152451905+MauriceLacourt@users.noreply.github.com>
2024-06-15 17:20:23 +0200

    Restructuration

--- Sous dossier  pj/Bonjour dans sous dossier.py
+++
@@ -1,9 +0,0 @@
-print("Bonjour, monde !")
-livre = "Gatsby le Magnifique"
-print ("coucou101")
-print ("coucou6")
-print ("coucou9999")
-print ("Philippe")
-print ("Martin")
-print ("pifou")
-print ("pifou3")
